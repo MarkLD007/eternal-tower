4ae57bf91e4daec97ae8c5a68424d0cb45a20395
hhhqqqxxx <155963161+hhhqqqxxx@users.noreply.github.com>
2025-04-09 23:05:28 +0800

    as

--- 伊特勒斯之塔/Assets/Script/Player/Sword.cs
+++ 伊特勒斯之塔/Assets/Script/Player/Sword.cs
@@ -9,7 +9,6 @@ using static UnityEngine.GraphicsBuffer;
 public class Sword : MonoBehaviour
 {
     public int shangHai;
-    public int distense;
     public int abstractHp;//影响上限血量
     public int substanceHp;//影响下限血量
     public float inSpeed;//影响速度
@@ -19,16 +18,7 @@ public class Sword : MonoBehaviour
     {
         healthBar = GameObject.Find("healthBar").GetComponent<HealthBar>();
     }
-   void limit()
-    {
-        GameObject player = gameObject.GetComponent<Transform>().parent.gameObject;
-        Vector3 mp = gameObject.GetComponent<Transform>().position;
-        Vector3 pp=player.GetComponent<Transform>().position;
-        Vector3 w = new Vector3(mp.x - pp.x, mp.y - pp.y, mp.z - pp.z);
-        Vector2 lw = Vector3.Normalize(w);
-        float angle =math.degrees( math.atan2(lw.y , lw.x));
-        gameObject.GetComponent<Transform>().eulerAngles = new Vector3(0,0, angle-90);
-    }
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
      
--- 伊特勒斯之塔/Assets/Script/Player/WeaponAttack.cs
+++ 伊特勒斯之塔/Assets/Script/Player/WeaponAttack.cs
@@ -6,38 +6,45 @@ using UnityEngine;
 
 public class WeaponAttack : MonoBehaviour
 {
-    public int distense;
-    public float aspeed;
-    Boolean attck = true;
-    private void OnTriggerStay2D(Collider2D collision)
+    public float aspeed=1;
+  public  Boolean attck = true;
+    GameObject weapon;
+    float stime = -1;
+   
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Enemy"&&attck)
         {
+            stime = 0;
             attck = false;
             GameObject enemy = collision.gameObject;
-            GameObject weapon = gameObject.GetComponent<Transform>().parent.gameObject;
             Vector3 ep = enemy.GetComponent<Transform>().position;
             Vector3 wp = weapon.GetComponent<Transform>().position;
             Vector3 p = new Vector3(ep.x - wp.x, ep.y - wp.y, ep.z - wp.z);
-          
-        
+            weapon.GetComponent<Rigidbody2D>().AddForce(new Vector2(p.x * 500, p.y * 500));
+            weapon.GetComponent<BoxCollider2D>().enabled = true;
         }
-
     }
     void Start()
     {
-        
+        weapon = gameObject.GetComponent<Transform>().parent.gameObject;
     }
 
     // Update is called once per frame
     void Update()
     {
-        aspeed += Time.deltaTime;
-        if (aspeed >= 1)
+        if (stime >= 0)
+            stime += Time.deltaTime;
+        if (stime >= 0.5 && stime <= 0.55)
+        {
+            weapon.GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+            weapon.GetComponent<BoxCollider2D>().enabled = false;
+        }
+         
+        if (stime >= aspeed&&attck==false)
         {
             attck = true;
-            aspeed = 0;
-
+            stime = -1;
         }
 
     }
