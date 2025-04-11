18cd48c8626fee6c061f550b4ebbeb15b0ee5ae8
MarkLD007 <144927013+MarkLD007@users.noreply.github.com>
2025-04-11 20:46:12 +0800

    更新

--- 伊特勒斯之塔/Assets/Script/UI/WeaponSelect.cs
+++ 伊特勒斯之塔/Assets/Script/UI/WeaponSelect.cs
@@ -1,18 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WeaponSeclect : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button Weapon1;
+    public Button Weapon2;
+    public Button Weapon3;
+    public Button Weapon4;
+    public Button Weapon5;
+
+    public Animator animator; 
     void Start()
     {
-        
+        // GameObject.DontDestroyOnLoad(gameObject);
+        Weapon1.onClick.AddListener(LoadScene);
+        Weapon2.onClick.AddListener(LoadScene);
+        Weapon3.onClick.AddListener(LoadScene);
+        Weapon4.onClick.AddListener(LoadScene);
+        Weapon5.onClick.AddListener(LoadScene);
     }
 
-    // Update is called once per frame
-    void Update()
+
+    private void LoadScene()
     {
-        
+        StartCoroutine(LoadScene(3));
     }
+
+
+    IEnumerator LoadScene(int index)
+    {
+        animator.SetBool("Fadein", true);
+        animator.SetBool("Fadeout", false);
+
+        yield return new WaitForSeconds(1);
+
+        AsyncOperation async = SceneManager.LoadSceneAsync(index);
+        async.completed += OnloadedScene;
+    }
+
+    private void OnloadedScene(AsyncOperation obj)
+    {
+        animator.SetBool("Fadein", false);
+        animator.SetBool("Fadeout", true);
+    }
+
 }
